82f64e012778c933f2f6165ceffdad0d5cbd2e4f
saurabh234kumar <74293017+saurabh234kumar@users.noreply.github.com>
2020-12-01 21:11:03 +0400

    .

--- CustomerExperience/src/newpackage/Myclass.java
+++ CustomerExperience/src/newpackage/Myclass.java
@@ -180,20 +180,7 @@ public class Myclass {
 		   } }  
 		  }
 	  
-	/*	@Test
-		public void softAssert(){
-			SoftAssert softAssertion= new SoftAssert();
-			System.out.println("softAssert Method Was Started");
-			softAssertion.assertTrue(false);
-			System.out.println("softAssert Method Was Executed");
-			softAssertion.assertAll();
-		}
-		@Test
-		public void hardAssert(){
-			System.out.println("hardAssert Method Was Started");
-			Assert.assertTrue(false);
-			System.out.println("hardAssert Method Was Executed");
-		}*/
+
 		
 		@AfterMethod
 		public void postSignUp()
